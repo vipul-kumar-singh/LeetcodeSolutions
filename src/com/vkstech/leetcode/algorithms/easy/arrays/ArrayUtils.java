6f677f92effffed90f83c92a7de094f7c0fc4cef
Vipul Kumar Singh <vipul24.9669@gmail.com>
2023-07-03 21:24:32 +0530

    Build Array from Permutation

---
+++ src/com/vkstech/leetcode/algorithms/easy/arrays/ArrayUtils.java
@@ -0,0 +1,18 @@
+package com.vkstech.leetcode.algorithms.easy.arrays;
+
+public class ArrayUtils {
+
+    public static void printArr(Object[] arr){
+        for (Object o : arr){
+            System.out.print(o + ", ");
+        }
+        System.out.println();
+    }
+
+    public static void printArr(int[] arr){
+        for (Object o : arr){
+            System.out.print(o + ", ");
+        }
+        System.out.println();
+    }
+}
