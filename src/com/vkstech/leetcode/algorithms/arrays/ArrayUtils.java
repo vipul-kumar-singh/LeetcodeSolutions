c227d3e4d6abd09925e75a1a0935a6c161480143
Vipul Kumar Singh <vipul24.9669@gmail.com>
2023-08-06 19:52:59 +0530

    Added ArrayUtils

---
+++ src/com/vkstech/leetcode/algorithms/arrays/ArrayUtils.java
@@ -0,0 +1,12 @@
+package com.vkstech.leetcode.algorithms.arrays;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] arr : matrix) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
